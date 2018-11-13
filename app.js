fe91105da6d18228a7bc8da8d7d625edc260fb5c
Chris Czach <chrisczach@gmail.com>
2018-11-13 09:51:03 -0800

    add keys

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-const request = require('request');
+const request = require("request");
+const fs = require("fs");
+const keys = JSON.parse(fs.readFileSync("./keys.json"));
 
-request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
-  json: true
-}, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-});
+request(
+  {
+    url: `https://maps.googleapis.com/maps/api/geocode/json?key=${
+      keys.gmaps
+    }&address=1301%20lombard%20street%20philadelphia`,
+    json: true
+  },
+  (error, response, body) => {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  }
+);
